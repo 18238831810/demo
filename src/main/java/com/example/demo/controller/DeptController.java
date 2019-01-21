57e6b655392f3aeab31d91ea03f79c985f5e2c8f
frank <1527583922@qq.com>
2019-01-22 00:12:59 +0800

    springboot去除监管机制

--- src/main/java/com/example/demo/controller/DeptController.java
+++ src/main/java/com/example/demo/controller/DeptController.java
@@ -6,10 +6,16 @@ import com.example.demo.mapper.DepartmentMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @RestController
@@ -18,12 +24,39 @@ public class DeptController {
     @Autowired
     DepartmentMapper departmentMapper;
 
+    @Autowired
+    RestTemplate restTemplate;
+
     Logger log = LoggerFactory.getLogger(getClass());
 
     @GetMapping("/dept/{id}")
     public Map<?, ?> getDepartment(@PathVariable("id") Integer id){
         log.info("id:[{}]",id);
         return departmentMapper.getDeptById(id);
+    }
+    @GetMapping("/")
+    public String index(){
+        return "account/pc/real_next";
+    }
+    @PostMapping("/dept/sety")
+    public String getTest(Integer id){
+        System.out.println(id);
+        //System.out.println(ip);
+        return "";
+    }
+    @GetMapping("/dept/test")
+    public ResponseEntity getwseTest(){
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        MultiValueMap<String, Object> map= new LinkedMultiValueMap<String, Object>();
+        HashMap<Object, Object> objectObjectHashMap = new HashMap<>();
+        objectObjectHashMap.put("ip","kjlllkl");
+        map.add("id", "156");
+        map.add("ip",objectObjectHashMap);
+        HttpEntity<MultiValueMap<String, Object>> request = new HttpEntity<MultiValueMap<String, Object>>(map, headers);
+        ResponseEntity<Object> responseEntity = restTemplate.postForEntity("http://localhost:8080/dept/sety", request, Object.class);
+        return responseEntity;
     }
 
 }
